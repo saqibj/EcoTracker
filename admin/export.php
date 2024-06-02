3992eba6ae2b212c8d2f081af9931d765f065af1
saqibj <jawaidovich@gmail.com>
2024-06-02 13:30:15 +0500

    The Purge

--- admin/export.php
+++
@@ -1,9 +0,0 @@
-<div class="wrap">
-    <h1>Export Projects</h1>
-    <form method="post" action="<?php echo esc_url(admin_url('admin-post.php')); ?>">
-        <input type="hidden" name="action" value="ecopower_tracker_export_csv">
-        <p class="submit">
-            <input type="submit" name="ecopower_tracker_export_submit" id="submit" class="button button-primary" value="Export CSV">
-        </p>
-    </form>
-</div>
--- templates/project-view.php
+++
@@ -1,8 +0,0 @@
-<div class="wrap">
-    <h1>Project View</h1>
-    <!-- Code to display individual project details -->
-    <?php
-    // Fetch project data based on ID or other identifier
-    // Display project details in a structured format
-    ?>
-</div>
